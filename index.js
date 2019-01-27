fc398d8496e8fc264bb767499713a92346c255bd
Owen Smith <owen@omsmith.ca>
2019-01-27 08:43:21 -0500

    update jws.ALGORITHMS

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var VerifyStream = require('./lib/verify-stream');
 var ALGORITHMS = [
   'HS256', 'HS384', 'HS512',
   'RS256', 'RS384', 'RS512',
+  'PS256', 'PS384', 'PS512',
   'ES256', 'ES384', 'ES512'
 ];
 
