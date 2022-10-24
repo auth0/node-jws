615355e99a1458d1f94c61d4b3774c3c64211f8f
Daniel Hensby <git@hens.by>
2022-10-24 16:29:54 +0100

    Refactor verify/sign to return promises

--- test/jws.test.js
+++ test/jws.test.js
@@ -54,18 +54,25 @@ BITS.forEach(function (bits) {
     const alg = 'HS'+bits;
     const header = { alg: alg, typ: 'JWT' };
     const secret = 'sup';
-    const jwsObj = jws.sign({
+    jws.sign({
       header: header,
       payload: payload,
       secret: secret,
       encoding: 'utf8',
+    }).then(function (jwsObj) {
+      return jws.verify(jwsObj, alg, secret).then(function (res) {
+        t.ok(res, 'should verify');
+        return jws.verify(jwsObj, alg, 'something else');
+      }).then(function (res) {
+        const parts = jws.decode(jwsObj);
+        t.notOk(res, 'should not verify with non-matching secret');
+        t.same(parts.payload, payload, 'should match payload');
+        t.same(parts.header, header, 'should match header');
+        t.end();
+      }).catch(function (e) {
+        t.end(e);
+      });
     });
-    const parts = jws.decode(jwsObj);
-    t.ok(jws.verify(jwsObj, alg, secret), 'should verify');
-    t.notOk(jws.verify(jwsObj, alg, 'something else'), 'should not verify with non-matching secret');
-    t.same(parts.payload, payload, 'should match payload');
-    t.same(parts.header, header, 'should match header');
-    t.end();
   });
 });
 
@@ -76,18 +83,28 @@ BITS.forEach(function (bits) {
     const privateKey = rsaPrivateKey;
     const publicKey = rsaPublicKey;
     const wrongPublicKey = rsaWrongPublicKey;
-    const jwsObj = jws.sign({
+    jws.sign({
       header: header,
       payload: payload,
       privateKey: privateKey
+    }).then(function (jwsObj) {
+      return jws.verify(jwsObj, alg, publicKey).then(function (res) {
+        t.ok(res, 'should verify');
+        return jws.verify(jwsObj, alg, wrongPublicKey);
+      }).then(function (res) {
+        t.notOk(res, 'should not verify with non-matching public key');
+        return jws.verify(jwsObj, 'HS' + bits, publicKey);
+      }).then(function (res) {
+        t.notOk(res, 'should not verify with non-matching algorithm');
+      }).then(function () {
+        const parts = jws.decode(jwsObj, { json: true });
+        t.same(parts.payload, payload, 'should match payload');
+        t.same(parts.header, header, 'should match header');
+        t.end();
+      });
+    }).catch(function (e) {
+      t.end(e);
     });
-    const parts = jws.decode(jwsObj, { json: true });
-    t.ok(jws.verify(jwsObj, alg, publicKey), 'should verify');
-    t.notOk(jws.verify(jwsObj, alg, wrongPublicKey), 'should not verify with non-matching public key');
-    t.notOk(jws.verify(jwsObj, 'HS'+bits, publicKey), 'should not verify with non-matching algorithm');
-    t.same(parts.payload, payload, 'should match payload');
-    t.same(parts.header, header, 'should match header');
-    t.end();
   });
 });
 
@@ -99,18 +116,27 @@ BITS.forEach(function (bits) {
     const privateKey = ecdsaPrivateKey[bits];
     const publicKey = ecdsaPublicKey[bits];
     const wrongPublicKey = ecdsaWrongPublicKey[bits];
-    const jwsObj = jws.sign({
+    jws.sign({
       header: header,
       payload: payloadString,
       privateKey: privateKey
+    }).then(function (jwsObj) {
+      return jws.verify(jwsObj, alg, publicKey).then(function (res) {
+        t.ok(res, 'should verify');
+        return jws.verify(jwsObj, alg, wrongPublicKey);
+      }).then(function (res) {
+        t.notOk(res, 'should not verify with non-matching public key');
+        return jws.verify(jwsObj, 'HS' + bits, publicKey);
+      }).then(function (res) {
+        const parts = jws.decode(jwsObj);
+        t.notOk(res, 'should not verify with non-matching algorithm');
+        t.same(parts.payload, payloadString, 'should match payload');
+        t.same(parts.header, header, 'should match header');
+        t.end();
+      });
+    }).catch(function (e) {
+      t.end(e);
     });
-    const parts = jws.decode(jwsObj);
-    t.ok(jws.verify(jwsObj, alg, publicKey), 'should verify');
-    t.notOk(jws.verify(jwsObj, alg, wrongPublicKey), 'should not verify with non-matching public key');
-    t.notOk(jws.verify(jwsObj, 'HS'+bits, publicKey), 'should not verify with non-matching algorithm');
-    t.same(parts.payload, payloadString, 'should match payload');
-    t.same(parts.header, header, 'should match header');
-    t.end();
   });
 });
 
@@ -118,17 +144,26 @@ test('No digital signature or MAC value included', function (t) {
   const alg = 'none';
   const header = { alg: alg };
   const payload = 'oh hey José!';
-  const jwsObj = jws.sign({
+  jws.sign({
     header: header,
     payload: payload,
+  }).then(function (jwsObj) {
+    return jws.verify(jwsObj, alg).then(function (res) {
+      t.ok(res, 'should verify');
+      return jws.verify(jwsObj, alg, 'anything');
+    }).then(function (res) {
+      t.ok(res, 'should still verify');
+      return jws.verify(jwsObj, 'HS256', 'anything');
+    }).then(function (res) {
+      const parts = jws.decode(jwsObj);
+      t.notOk(res, 'should not verify with non-matching algorithm');
+      t.same(parts.payload, payload, 'should match payload');
+      t.same(parts.header, header, 'should match header');
+      t.end();
+    });
+  }).catch(function (e) {
+    t.end(e);
   });
-  const parts = jws.decode(jwsObj);
-  t.ok(jws.verify(jwsObj, alg), 'should verify');
-  t.ok(jws.verify(jwsObj, alg, 'anything'), 'should still verify');
-  t.notOk(jws.verify(jwsObj, 'HS256', 'anything'), 'should not verify with non-matching algorithm');
-  t.same(parts.payload, payload, 'should match payload');
-  t.same(parts.header, header, 'should match header');
-  t.end();
 });
 
 test('Streaming sign: HMAC', function (t) {
@@ -140,8 +175,15 @@ test('Streaming sign: HMAC', function (t) {
   });
   dataStream.pipe(sig.payload);
   sig.on('done', function (signature) {
-    t.ok(jws.verify(signature, 'HS256', secret), 'should verify');
-    t.end();
+    jws.verify(signature, 'HS256', secret).then(function (res) {
+      t.ok(res, 'should verify');
+      t.end();
+    }).catch(function (e) {
+      t.end(e);
+    });
+  });
+  sig.on('error', function (error) {
+    t.end(error);
   });
 });
 
@@ -160,10 +202,20 @@ test('Streaming sign: RSA', function (t) {
   });
 
   sig.on('done', function (signature) {
-    t.ok(jws.verify(signature, 'RS256', publicKey), 'should verify');
-    t.notOk(jws.verify(signature, 'RS256', wrongPublicKey), 'should not verify');
-    t.same(jws.decode(signature).payload, readfile('data.txt'), 'got all the data');
-    t.end();
+    jws.verify(signature, 'RS256', publicKey).then(function (res) {
+      t.ok(res, 'should verify');
+      return jws.verify(signature, 'RS256', wrongPublicKey);
+    }).then(function (res) {
+      t.notOk(res, 'should not verify');
+      t.same(jws.decode(signature).payload, readfile('data.txt'), 'got all the data');
+      t.end();
+    }).catch(function (e) {
+      t.end(e);
+    });
+  });
+
+  sig.on('error', function (error) {
+    t.end(error);
   });
 });
 
@@ -178,10 +230,19 @@ test('Streaming sign: RSA, predefined streams', function (t) {
     privateKey: privateKeyStream
   });
   sig.on('done', function (signature) {
-    t.ok(jws.verify(signature, 'RS256', publicKey), 'should verify');
-    t.notOk(jws.verify(signature, 'RS256', wrongPublicKey), 'should not verify');
-    t.same(jws.decode(signature).payload, readfile('data.txt'), 'got all the data');
-    t.end();
+    jws.verify(signature, 'RS256', publicKey).then(function (res) {
+      t.ok(res, 'should verify');
+      return jws.verify(signature, 'RS256', wrongPublicKey);
+    }).then(function (res) {
+      t.notOk(res, 'should not verify');
+      t.same(jws.decode(signature).payload, readfile('data.txt'), 'got all the data');
+      t.end();
+    }).catch(function (e) {
+      t.end(e);
+    });
+  });
+  sig.on('error', function (error) {
+    t.end(error);
   });
 });
 
@@ -200,6 +261,9 @@ test('Streaming verify: ECDSA', function (t) {
   verifier.on('done', function (valid) {
     t.ok(valid, 'should verify');
     t.end();
+  });
+  verifier.on('error', function (error) {
+    t.end(error);
   });
 });
 
@@ -221,6 +285,9 @@ test('Streaming verify: ECDSA, with invalid key', function (t) {
     t.notOk(valid, 'should not verify');
     t.end();
   });
+  verifier.on('error', function (error) {
+    t.end(error);
+  });
 });
 
 test('Streaming verify: errors during verify should emit as "error"', function (t) {
@@ -235,23 +302,26 @@ test('Streaming verify: errors during verify should emit as "error"', function (
     t.end();
   });
   verifierShouldError.on('error', function () {
-    t.end()
+    t.end();
   });
 });
 
 if (SUPPORTS_ENCRYPTED_KEYS) {
   test('Signing: should accept an encrypted key', function (t) {
     const alg = 'RS256';
-    const signature = jws.sign({
+    jws.sign({
       header: { alg: alg },
       payload: 'verifyme',
       privateKey: {
         key: rsaPrivateKeyEncrypted,
         passphrase: encryptedPassphrase
       }
+    }).then(function (signature) {
+      t.ok(jws.verify(signature, 'RS256', rsaPublicKey));
+      t.end();
+    }).catch(function (e) {
+      t.end(e);
     });
-    t.ok(jws.verify(signature, 'RS256', rsaPublicKey));
-    t.end();
   });
 
   test('Streaming sign: should accept an encrypted key', function (t) {
@@ -272,6 +342,9 @@ if (SUPPORTS_ENCRYPTED_KEYS) {
     verifier.on('done', function (verified) {
       t.ok(verified);
       t.end();
+    });
+    verifier.on('error', function (error) {
+      t.end(error);
     });
   });
 }
@@ -297,7 +370,7 @@ test('jws.decode: with invalid json in body', function (t) {
   const sig = header + '.' + payload + '.';
   t.throws(function () {
     jws.decode(sig);
-  })
+  });
   t.end();
 });
 
@@ -305,27 +378,84 @@ test('jws.verify: missing or invalid algorithm', function (t) {
   const header = Buffer.from('{"something":"not an algo"}').toString('base64');
   const payload = Buffer.from('sup').toString('base64');
   const sig = header + '.' + payload + '.';
-  try { jws.verify(sig) }
-  catch (e) {
+  jws.verify(sig).then(function () {
+    // should have rejected before this
+    t.fail();
+  }).catch(function (e) {
     t.same(e.code, 'MISSING_ALGORITHM');
-  }
-  try { jws.verify(sig, 'whatever') }
-  catch (e) {
+  }).then(function () {
+    return jws.verify(sig, 'whatever').then(function () {
+      // should have rejected before this
+      t.fail();
+    });
+  }).catch(function (e) {
     t.ok(e.message.match('"whatever" is not a valid algorithm.'));
-  }
-  t.end();
+    t.end();
+  }).catch(function (e) {
+    t.end(e);
+  });
 });
 
 
 test('jws.isValid', function (t) {
-  const valid = jws.sign({ header: { alg: 'HS256' }, payload: 'hi', secret: 'shhh' });
-  const invalid = (function(){
-    const header = Buffer.from('oh hei José!').toString('base64');
-    const payload = Buffer.from('sup').toString('base64');
-    return header + '.' + payload + '.';
-  })();
-  t.same(jws.isValid('http://sub.domain.org'), false);
-  t.same(jws.isValid(invalid), false);
-  t.same(jws.isValid(valid), true);
-  t.end();
+  jws.sign({ header: { alg: 'HS256' }, payload: 'hi', secret: 'shhh' }).then(function (valid) {
+    const invalid = (function () {
+      const header = Buffer.from('oh hei José!').toString('base64');
+      const payload = Buffer.from('sup').toString('base64');
+      return header + '.' + payload + '.';
+    })();
+    t.same(jws.isValid('http://sub.domain.org'), false);
+    t.same(jws.isValid(invalid), false);
+    t.same(jws.isValid(valid), true);
+    t.end();
+  }).catch(function (e) {
+    t.end(e);
+  });
+});
+
+test('custom jwa', function (t) {
+  var verifyCalls = [];
+  var signCalls = [];
+  function jwa(alg) {
+    if (alg === 'CUSTOM') {
+      return {
+        verify: function (payload, signature, secret) {
+          verifyCalls.push([payload, signature, secret]);
+          if (secret === 'shhh') {
+            return signature === 'SIGNED';
+          }
+          return false;
+        },
+        sign: function (payload, secret) {
+          signCalls.push([payload, secret]);
+          if (secret === 'shhh') {
+            return 'SIGNED';
+          }
+          throw new Error('unable to sign');
+        },
+      };
+    }
+  }
+  jws.sign({ header: { alg: 'CUSTOM' }, payload: 'hi', secret: 'shhh', jwa: jwa }).then(function (signature) {
+    t.same(signCalls.length, 1);
+    t.same(signature, 'eyJhbGciOiJDVVNUT00ifQ.aGk.SIGNED');
+    return jws.sign({ header: { alg: 'CUSTOM' }, payload: 'hi', secret: 'bad', jwa: jwa }).then(function () {
+      t.fail();
+    }).catch(function (e) {
+      t.same(signCalls.length, 2);
+      t.same(e.message, 'unable to sign');
+    });
+  }).then(function () {
+    return jws.verify('eyJhbGciOiJDVVNUT00ifQ.aGk.SIGNED', 'CUSTOM', 'shhh', { jwa: jwa });
+  }).then(function (valid) {
+    t.same(verifyCalls.length, 1);
+    t.ok(valid);
+    return jws.verify('eyJhbGciOiJDVVNUT00ifQ.aGk.invalid', 'CUSTOM', 'shhh', { jwa: jwa });
+  }).then(function (valid) {
+    t.same(verifyCalls.length, 2);
+    t.notOk(valid);
+    t.end();
+  }).catch(function (e) {
+    t.end(e);
+  });
 });
